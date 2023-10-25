e69d86ae6ac28694a8a89686913ffe4e4db2fb36
DevHola <Connectola@yahoo.com>
2023-10-24 20:21:14 -0700

    add delay

--- utils/db.js
+++ utils/db.js
@@ -4,8 +4,8 @@ const connection = () => {
   mongoose.connect(process.env.mongouri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    family: 4,
-    authSource: 'admin'
+    family: 4
+    // authSource: 'admin'
     // mongodb://adminUser:adminPassword@localhost:27017/?authMechanism=DEFAULT&authSource=ana
   }).then(() => {
     console.log('Connection Established')
