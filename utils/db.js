5d2366b0a03904bea1debdb013a59fbb9b6010c3
DevHola <Connectola@yahoo.com>
2023-10-24 22:48:07 -0700

    mongourl updated

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const connection = () => {
-  mongoose.connect('mongodb://adminUser:adminPassword@mongodb:27017/?authMechanism=DEFAULT&authSource=ana', {
+  mongoose.connect(process.env.mongouri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4,
-    authSource: 'admin'
-    // mongodb://adminUser:adminPassword@localhost:27017/?authMechanism=DEFAULT&authSource=admin
+    authSource: 'ana'
+    // mongodb://adminUser:adminPassword@localhost:27017/?authMechanism=DEFAULT&authSource=ana
   }).then(() => {
     console.log('Connection Established')
   }).catch((error) => {
