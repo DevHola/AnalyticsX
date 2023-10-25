b9c9a57cd17faf39e4dad9e72085b7d2d8e55f76
DevHola <Connectola@yahoo.com>
2023-10-24 21:44:58 -0700

    mongourl updated

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const connection = () => {
   mongoose.connect(process.env.mongouri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    family: 4,
-    authSource: 'admin'
-    // mongodb://adminUser:adminPassword@localhost:27017/?authMechanism=DEFAULT&authSource=ana
+    useUnifiedTopology: true
+    // family: 4,
+    // useCreateIndex: true,
+    // useFindAndModify: false
+    // mongodb://adminUser:adminPassword@localhost:27017/?directConnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true
   }).then(() => {
     console.log('Connection Established')
   }).catch((error) => {
